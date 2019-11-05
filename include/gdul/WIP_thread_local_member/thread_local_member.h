f9169f78558ebd1b8c56a8cc64ebc3accae3fa77
Flovin Tue Michaelsen <flovintue@outlook.com>
2019-11-05 17:00:55 +0100

    Finished trial version of thread_local_member

--- include/gdul/WIP_thread_local_member/thread_local_member.h
+++ include/gdul/WIP_thread_local_member/thread_local_member.h
@@ -6,13 +6,18 @@
 
 namespace gdul
 {
+template <class T, class Allocator = std::allocator<T>>
+class thread_local_member;
+
+template <class T, class Allocator = std::allocator<T>>
+using tlm = thread_local_member<T, Allocator>;
 
 namespace thread_local_member_detail
 {
 static constexpr std::size_t Static_Alloc_Size = 4;
 
 template <class T, class Allocator>
-union alignas(alignof(T) < 8 ? 8 : alignof(T)) flexible_storage;
+class alignas(alignof(T) < 8 ? 8 : alignof(T)) flexible_storage;
 
 template <class IndexType, class Allocator>
 class index_pool;
@@ -30,89 +35,177 @@ public:
 	operator T& ();
 	operator T& () const;
 
+	template <std::enable_if_t<std::is_move_assignable_v<T>>* = nullptr>
+	const T& operator=(T&& other);
+
+	template <std::enable_if_t<std::is_copy_assignable_v<T>>* = nullptr>
+	const T& operator=(const T& other);
+
 private:
+	using allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<T>;
 
-	T* get_array_ref();
-
-	static thread_local thread_local_member_detail::flexible_storage s_storage;
-	static thread_local T* s_arrayRef;
+	static thread_local thread_local_member_detail::flexible_storage<T, Allocator> s_storage;
 	static thread_local_member_detail::index_pool<std::uint32_t, Allocator> s_indexPool;
 
+	allocator_type m_allocator;
 	const std::uint32_t m_instanceIndex;
-	Allocator m_allocator;
 };
 
 template<class T, class Allocator>
 inline thread_local_member<T, Allocator>::thread_local_member()
+	: m_instanceIndex(s_indexPool.get(m_allocator))
 {
 }
-
 template<class T, class Allocator>
 inline thread_local_member<T, Allocator>::thread_local_member(Allocator& allocator)
 	: m_allocator(allocator)
+	, m_instanceIndex(s_indexPool.get(m_allocator))
 {
 }
-
 template<class T, class Allocator>
 inline thread_local_member<T, Allocator>::~thread_local_member()
 {
+	s_indexPool.add(m_instanceIndex);
 }
-
-
 template<class T, class Allocator>
 inline thread_local_member<T, Allocator>::operator T& ()
 {
+	return s_storage.get_index(m_instanceIndex, m_allocator);
 }
 template<class T, class Allocator>
 inline thread_local_member<T, Allocator>::operator T& () const
 {
+	return s_storage.get_index(m_instanceIndex, m_allocator);
+}
+
+template<class T, class Allocator>
+template <std::enable_if_t<std::is_copy_assignable_v<T>>*>
+inline const T& thread_local_member<T, Allocator>::operator=(const T& other)
+{
+	T& myval(*this);
+	myval = other;
+	return *this;
 }
 template<class T, class Allocator>
-inline T* thread_local_member<T, Allocator>::get_array_ref()
+template<std::enable_if_t<std::is_move_assignable_v<T>>*>
+inline const T& thread_local_member<T, Allocator>::operator=(T&& other)
 {
-	return NULL;
+	T& myval(*this);
+	myval = std::move(other);
+	return *this;
 }
 
 // detail
 namespace thread_local_member_detail
 {
 template <class T, class Allocator>
-union alignas(alignof(T) < 8 ? 8 : alignof(T)) flexible_storage
+class alignas(alignof(T) < 8 ? 8 : alignof(T)) flexible_storage
 {
+public:
 	flexible_storage();
+	~flexible_storage();
+
+	T& get_index(std::uint32_t index, Allocator & allocator);
+
+private:
+
+	T* get_array_ref(std::uint32_t capacity, Allocator & allocator);
+	void grow_to_size(std::uint32_t capacity, Allocator & allocator);
 
 	void construct_static();
-	void construct_dynamic(Allocator allocator);
+	void construct_dynamic(Allocator& allocator);
 	void destroy_static();
-	void destroy_dynamic(Allocator allocator);
-
-	static thread_local std::size_t s_capacity;
+	void destroy_dynamic();
 
 	std::array<T, thread_local_member_detail::Static_Alloc_Size>& get_static();
 	std::vector<T, Allocator>& get_dynamic();
 
-	std::uint8_t myStaticStorage[sizeof(std::array<T, thread_local_member_detail::Static_Alloc_Size>];
-	std::uint8_t myDynamicStorage[sizeof(std::vector<T, Allocator>)];
+	union
+	{
+		std::uint8_t m_staticStorage[sizeof(std::array<T, thread_local_member_detail::Static_Alloc_Size>)];
+		std::uint8_t m_dynamicStorage[sizeof(std::vector<T, Allocator>)];
+	};
+	T* m_arrayRef;
+	std::uint32_t m_capacity;
 };
 
 template<class T, class Allocator>
 inline flexible_storage<T, Allocator>::flexible_storage()
+	: m_staticStorage{}
+	, m_capacity(0)
+	, m_arrayRef(nullptr)
 {
-	construct_static();
+}
+template<class T, class Allocator>
+inline flexible_storage<T, Allocator>::~flexible_storage()
+{
+	if (!(Static_Alloc_Size < m_capacity)) {
+		destroy_static();
+	}
+	else {
+		destroy_dynamic();
+	}
+}
+template<class T, class Allocator>
+inline T& flexible_storage<T, Allocator>::get_index(std::uint32_t index, Allocator& allocator)
+{
+	if (!(index < m_capacity)) {
+		m_arrayRef = get_array_ref(index + 1, allocator);
+	}
+	return m_arrayRef[index];
+}
+template<class T, class Allocator>
+inline T* flexible_storage<T, Allocator>::get_array_ref(std::uint32_t capacity, Allocator& allocator)
+{
+	if (m_capacity < capacity) {
+		grow_to_size(capacity, allocator);
+	}
+	if (!(Static_Alloc_Size < m_capacity)) {
+		return &get_static()[0];
+	}
+
+	return &get_dynamic()[0];
+}
+template<class T, class Allocator>
+inline void flexible_storage<T, Allocator>::grow_to_size(std::uint32_t capacity, Allocator& allocator)
+{
+	if (!(Static_Alloc_Size < capacity)) {
+		if (!m_capacity) {
+			construct_static();
+		}
+	}
+	else{
+		if (!(Static_Alloc_Size < m_capacity)) {
+			construct_dynamic(allocator);
+		}
+
+		get_dynamic().resize(capacity);
+
+	}
+	m_capacity = capacity;
 }
 template<class T, class Allocator>
 inline void flexible_storage<T, Allocator>::construct_static()
 {
-	new ((std::array<T, Static_Alloc_Size>*) & myStaticStorage[0]) std::array<T, Static_Alloc_Size>();
+	new ((std::array<T, Static_Alloc_Size>*) & m_staticStorage[0]) std::array<T, Static_Alloc_Size>();
 }
 template<class T, class Allocator>
-inline void flexible_storage<T, Allocator>::construct_dynamic(Allocator allocator)
+inline void flexible_storage<T, Allocator>::construct_dynamic(Allocator& allocator)
 {
-	std::array<T, Static_Alloc_Size> move(std::move(get_static()));
+	std::array<T, Static_Alloc_Size> src;
 
-	destroy_static();
+	if (m_capacity) {
+		src = std::move(get_static());
 
-	new ((std::vector<T, Allocator>*) & myDynamicStorage[0]) std::vector<T, Allocator>(allocator);
+		destroy_static();
+	}
+	
+	new ((std::vector<T, Allocator>*) & m_dynamicStorage[0]) std::vector<T, Allocator>(m_capacity, allocator);
+	get_dynamic().resize(0);
+
+	for (std::size_t i = 0; i < m_capacity; ++i) {
+		get_dynamic().emplace_back(std::move(src[i]));
+	}
 }
 template<class T, class Allocator>
 inline void flexible_storage<T, Allocator>::destroy_static()
@@ -120,19 +213,19 @@ inline void flexible_storage<T, Allocator>::destroy_static()
 	get_static().~array();
 }
 template<class T, class Allocator>
-inline void flexible_storage<T, Allocator>::destroy_dynamic(Allocator allocator)
+inline void flexible_storage<T, Allocator>::destroy_dynamic()
 {
 	get_dynamic().~vector();
 }
 template<class T, class Allocator>
 inline std::array<T, thread_local_member_detail::Static_Alloc_Size>& flexible_storage<T, Allocator>::get_static()
 {
-	return *reinterpret_cast<std::array<T, Static_Alloc_Size>*>(&myStaticStorage[0]);
+	return *reinterpret_cast<std::array<T, Static_Alloc_Size>*>(&m_staticStorage[0]);
 }
 template<class T, class Allocator>
 inline std::vector<T, Allocator>& flexible_storage<T, Allocator>::get_dynamic()
 {
-	return *reinterpret_cast<std::vector<T, Allocator>*>(&myDynamicStorage[0]);
+	return *reinterpret_cast<std::vector<T, Allocator>*>(&m_dynamicStorage[0]);
 }
 template <class IndexType, class Allocator>
 class index_pool
@@ -147,14 +240,13 @@ public:
 	struct node
 	{
 		node(IndexType index, shared_ptr<node> next)
-			: myIndex(index)
-			, myNext(std::move(next))
-		{
-		}
-		IndexType myIndex;
-		atomic_shared_ptr<node> myNext;
+			: m_index(index)
+			, m_next(std::move(next))
+		{}
+		IndexType m_index;
+		atomic_shared_ptr<node> m_next;
 	};
-	atomic_shared_ptr<node> myTop;
+	atomic_shared_ptr<node> m_top;
 
 private:
 	// Pre-allocate 'return entry' (no allocations in destructor)
@@ -162,35 +254,35 @@ private:
 	void push_pool_entry(shared_ptr<node> node);
 	shared_ptr<node> get_pool_entry();
 
-	atomic_shared_ptr<node> myTopPool;
+	atomic_shared_ptr<node> m_topPool;
 
-	std::atomic<IndexType> myIterator;
+	std::atomic<IndexType> m_iterator;
 };
 template<class IndexType, class Allocator>
 inline index_pool<IndexType, Allocator>::index_pool()
-	: myTop(nullptr)
-	, myIterator(0)
+	: m_top(nullptr)
+	, m_iterator(0)
 {
 }
 template<class IndexType, class Allocator>
 inline index_pool<IndexType, Allocator>::~index_pool()
 {
-	shared_ptr<node> top(myTop.unsafe_load());
+	shared_ptr<node> top(m_top.unsafe_load());
 	while (top) {
-		shared_ptr<node> next(top->myNext.unsafe_load());
-		myTop.unsafe_store(next);
+		shared_ptr<node> next(top->m_next.unsafe_load());
+		m_top.unsafe_store(next);
 		top = std::move(next);
 	}
 }
 template<class IndexType, class Allocator>
 inline IndexType index_pool<IndexType, Allocator>::get(Allocator& allocator)
 {
-	shared_ptr<node> top(myTop.load(std::memory_order_relaxed));
+	shared_ptr<node> top(m_top.load(std::memory_order_relaxed));
 	while (top) {
 		raw_ptr<node> expected(top);
-		if (myTop.compare_exchange_strong(expected, top->myNext.load(std::memory_order_acquire), std::memory_order_relaxed)) {
+		if (m_top.compare_exchange_strong(expected, top->m_next.load(std::memory_order_acquire), std::memory_order_relaxed)) {
 
-			const IndexType toReturn(top->myIndex);
+			const IndexType toReturn(top->m_index);
 
 			push_pool_entry(std::move(top));
 
@@ -200,20 +292,20 @@ inline IndexType index_pool<IndexType, Allocator>::get(Allocator& allocator)
 
 	push_pool_entry(allocator);
 
-	return myIterator++;
+	return m_iterator.fetch_add(1, std::memory_order_relaxed);
 }
 template<class IndexType, class Allocator>
 inline void index_pool<IndexType, Allocator>::add(IndexType index)
 {
 	shared_ptr<node> entry(get_pool_entry());
-	entry->myIndex = index;
+	entry->m_index = index;
 
 	raw_ptr<node> expected;
 	do {
-		shared_ptr<node> top(myTop.load());
+		shared_ptr<node> top(m_top.load());
 		expected = top.get_raw_ptr();
-		entry->myNext.unsafe_store(std::move(top));
-	} while (!myTop.compare_exchange_strong(expected, std::move(entry)));
+		entry->m_next.unsafe_store(std::move(top));
+	} while (!m_top.compare_exchange_strong(expected, std::move(entry)));
 }
 template<class IndexType, class Allocator>
 inline void index_pool<IndexType, Allocator>::push_pool_entry(Allocator& allocator)
@@ -229,22 +321,27 @@ inline void index_pool<IndexType, Allocator>::push_pool_entry(shared_ptr<node> e
 
 	raw_ptr<node> expected;
 	do {
-		shared_ptr<node> top(myTopPool.load(std::memory_order_acquire));
+		shared_ptr<node> top(m_topPool.load(std::memory_order_acquire));
 		expected = top.get_raw_ptr();
-		toInsert->myNext = std::move(top);
-	} while (!myTopPool.compare_exchange_strong(expected, std::move(toInsert)));
+		toInsert->m_next = std::move(top);
+	} while (!m_topPool.compare_exchange_strong(expected, std::move(toInsert)));
 }
 template<class IndexType, class Allocator>
 inline shared_ptr<typename index_pool<IndexType, Allocator>::node> index_pool<IndexType, Allocator>::get_pool_entry()
 {
-	shared_ptr<node> top(myTopPool.load(std::memory_order_relaxed));
+	shared_ptr<node> top(m_topPool.load(std::memory_order_relaxed));
 	while (top) {
 		raw_ptr<node> expected(top);
-		if (myTopPool.compare_exchange_strong(expected, top->myNext.load(std::memory_order_acquire), std::memory_order_relaxed)) {
+		if (m_topPool.compare_exchange_strong(expected, top->m_next.load(std::memory_order_acquire), std::memory_order_relaxed)) {
 			return top;
 		}
 	}
 	throw std::runtime_error("Pre allocated entries should be 1:1 to fetched indices");
 }
 }
+template <class T, class Allocator>
+thread_local_member_detail::index_pool<std::uint32_t, Allocator> thread_local_member<T, Allocator>::s_indexPool;
+template <class T, class Allocator>
+thread_local thread_local_member_detail::flexible_storage<T, Allocator> thread_local_member<T, Allocator>::s_storage;
+
 }
--- include/gdul/concurrent_queue/concurrent_queue.h
+++ include/gdul/concurrent_queue/concurrent_queue.h
@@ -32,17 +32,18 @@
 // a slight performance decrease
 
 
-/* #define GDUL_CQ_ENABLE_EXCEPTIONHANDLING */
+#define GDUL_CQ_ENABLE_EXCEPTIONHANDLING 
 
 // In the event an exception is thrown during a pop operation, some entries may
 // be dequeued out-of-order as some consumers may already be halfway through a 
 // pop operation before reintegration efforts are started.
 
+
 #ifdef GDUL_CQ_ENABLE_EXCEPTIONHANDLING 
 #define GDUL_CQ_BUFFER_NOTHROW_POP_MOVE(type) (std::is_nothrow_move_assignable<type>::value)
-#define GDUL_CQ_BUFFER_NOTHROW_POP_ASSIGN(type) (!GDUL_CQ_BUFFER_NOTHROW_POP_MOVE(type) && (std::is_nothrow_assignable<type, type&>::value))
+#define GDUL_CQ_BUFFER_NOTHROW_POP_ASSIGN(type) (!GDUL_CQ_BUFFER_NOTHROW_POP_MOVE(type) && (std::is_nothrow_copy_assignable<type>::value))
 #define GDUL_CQ_BUFFER_NOTHROW_PUSH_MOVE(type) (std::is_nothrow_move_assignable<type>::value)
-#define GDUL_CQ_BUFFER_NOTHROW_PUSH_ASSIGN(type) (std::is_nothrow_assignable<type, type&>::value)
+#define GDUL_CQ_BUFFER_NOTHROW_PUSH_ASSIGN(type) (std::is_nothrow_copy_assignable<type>::value)
 #else
 #define GDUL_CQ_BUFFER_NOTHROW_POP_MOVE(type) (std::is_move_assignable<type>::value)
 #define GDUL_CQ_BUFFER_NOTHROW_POP_ASSIGN(type) (!GDUL_CQ_BUFFER_NOTHROW_POP_MOVE(type))
