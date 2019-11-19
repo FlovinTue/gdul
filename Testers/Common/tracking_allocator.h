60c0326887a8a5e33a4e2dacef657211666bd834
Flovin Tue Michaelsen <flovintue@outlook.com>
2019-11-19 08:56:24 +0100

    Still some wonk with index_pool

--- Testers/Common/tracking_allocator.h
+++ Testers/Common/tracking_allocator.h
@@ -6,7 +6,7 @@
 namespace gdul{
 	
 static std::atomic_flag s_spin{ ATOMIC_FLAG_INIT };
-static int64_t s_allocated(0);
+static std::atomic<int64_t> s_allocated(0);
 template <class T>
 class tracking_allocator : public std::allocator<T>
 {
@@ -24,7 +24,7 @@ public:
 	T* allocate(std::size_t count)
 	{
 		T* ret = std::allocator<T>::allocate(count);
-		spin();
+		//spin();
 		s_allocated += count * sizeof(T);
 		//std::cout << "allocated " << count * sizeof(T) << "--------------" << " new value: " << s_allocated << std::endl;
 		release();
@@ -33,10 +33,10 @@ public:
 	void deallocate(T* obj, std::size_t count)
 	{
 		std::allocator<T>::deallocate(obj, count);
-		spin();
+		//spin();
 		s_allocated -= count * sizeof(T);
 		//std::cout << "deallocated " << count * sizeof(T) << "--------------" << " new value: " << s_allocated << std::endl;
-		release();
+		//release();
 	}
 	void spin()
 	{
