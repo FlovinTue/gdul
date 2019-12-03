e384e308c66dda1daaaeef33c2ed72a00611df2d
Flovin Tue Michaelsen <flovintue@outlook.com>
2019-12-03 17:03:13 +0100

    Sketching on scatter_job

--- include/gdul/WIP_job_handler/scatter_job.h
+++ include/gdul/WIP_job_handler/scatter_job.h
@@ -2,6 +2,7 @@
 
 #include <gdul/WIP_job_handler/job_handler_commons.h>
 #include <gdul/WIP_job_handler/job_delegate.h>
+#include <gdul/WIP_job_handler/job_handler.h>
 #include <vector>
 
 namespace gdul
@@ -23,11 +24,56 @@ template <class T>
 class scatter_job
 {
 public:
-	scatter_job(std::vector<T>& dataSource, std::vector<T>& dataTarget, job_delegate process, std::size_t jobs);
+	scatter_job(std::vector<T>& dataSource, std::vector<T>& dataTarget, std::size_t chunkSize, job_delegate process);
 
 private:
+	void prepare_data();
+	void work_func(std::uint32_t begin, std::uint32_t end);
+	void enqueue_jobs();
+	void collect();
+
 	std::vector<T>& m_scatter;
-	std::vector<T>& m_gather;
-	
+	std::vector<T*>& m_gather;
+	job_delegate m_process;
 };
+
+template<class T>
+inline void scatter_job<T>::enqueue_jobs()
+{
+	std::uint32_t begin;
+	std::uint32_t end;
+	job newjob(job_handler::make_job(&scatter_job::work_func, this, begin, end));
+}
+template<class T>
+inline void scatter_job<T>::collect()
+{
+	std::vector<T>::iterator fIter(m_gather.begin());
+	std::vector<T>::reverse_iterator rIter(m_gather.end());
+
+	for (; fIter != rIter; ++fIter){
+		if (*fIter){
+			continue;
+		}
+		if (*rIter){
+			std::swap(*fIter, *rIter);
+		}
+
+		--rIter;
+	}
+}
+template<class T>
+inline void scatter_job<T>::prepare_data()
+{
+	m_gather.resize(m_scatter.size());
+	std::uninitialized_fill(m_gather.begin(), m_gather.end(), nullptr);
+}
+template<class T>
+inline void scatter_job<T>::work_func(std::uint32_t begin, std::uint32_t end)
+{
+	for (std::uint32_t i = begin; i < end; ++i){
+		if (m_process(m_scatter[i])){
+			m_gather[i] = &m_scatter[i];
+		}
+	}
+}
 }
