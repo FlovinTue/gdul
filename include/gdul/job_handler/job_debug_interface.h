be9199a2bda0099ecfe72c1d0649362f4a163b79
Flovin Tue Michaelsen <flovintue@outlook.com>
2020-02-24 09:54:07 +0100

    Swapped __COUNTER__ for file hash + line. Inlined some pragmas. Neated up.

--- include/gdul/job_handler/job_debug_interface.h
+++ include/gdul/job_handler/job_debug_interface.h
@@ -1,3 +1,23 @@
+// Copyright(c) 2019 Flovin Michaelsen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <gdul/job_handler/globals.h>
@@ -5,6 +25,9 @@
 #if defined(GDUL_JOB_DEBUG)
 #include <gdul/job_handler/job_debug_tracker.h>
 #endif
+
+
+
 
 namespace gdul {
 namespace jh_detail {
@@ -41,6 +64,17 @@ private:
 // A little trick to squeeze in a macro. To enable compile time enumeration of declarations
 #if !defined (activate_debug_tracking)
 #if defined(GDUL_JOB_DEBUG)
-#define activate_debug_tracking(name) activate_debug_tracking(name, GDUL_CEXPR_ID())
+#if  defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#define GDUL_INLINE_PRAGMA(pragma) __pragma(pragma)
+#else
+#define GDUL_STRINGIFY_PRAGMA(pragma) #pragma
+#define GDUL_INLINE_PRAGMA(pragma) _Pragma(GDUL_STRINGIFY_PRAGMA(pragma))
+#endif
+#define activate_debug_tracking(name) activate_debug_tracking(name, gdul::constexpr_id::make< \
+GDUL_INLINE_PRAGMA(warning(push)) \
+GDUL_INLINE_PRAGMA(warning(disable : 4307)) \
+constexp_str_hash(__FILE__) \
+GDUL_INLINE_PRAGMA(warning(pop)) \
++ std::size_t(__LINE__)>())
 #endif
 #endif
--- include/gdul/job_handler/job_debug_tracker.h
+++ include/gdul/job_handler/job_debug_tracker.h
@@ -1,3 +1,23 @@
+// Copyright(c) 2019 Flovin Michaelsen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <gdul/job_handler/globals.h>
@@ -6,8 +26,19 @@
 
 #include <type_traits>
 
-#define GDUL_CEXPR_ID_COUNTER __COU##NTER__
-#define GDUL_CEXPR_ID() gdul::constexpr_id::make<GDUL_CEXPR_ID_COUNTER>()
+// https://stackoverflow.com/questions/48896142/is-it-possible-to-get-hash-values-as-compile-time-constants
+template <typename Str>
+constexpr size_t constexp_str_hash(const Str& toHash)
+{
+	size_t result = 0xcbf29ce484222325ull; 
+
+	for (char c : toHash) {
+		result ^= c;
+		result *= 1099511628211ull;
+	}
+
+	return result;
+}
 
 namespace gdul {
 class job;
