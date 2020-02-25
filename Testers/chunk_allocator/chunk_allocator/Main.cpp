5a3568afffc8ec644a17e3824922a7eea710c4ca
Flovin Tue Michaelsen <flovintue@outlook.com>
2020-02-25 17:01:18 +0100

    Added new idea file

--- Testers/chunk_allocator/chunk_allocator/Main.cpp
+++ Testers/chunk_allocator/chunk_allocator/Main.cpp
@@ -9,13 +9,14 @@
 struct alignas(32) test_object
 {
 	int m_mem;
+	int m_nonsense[20];
 };
 
 int main()
 {
 	gdul::allocate_shared_chunk_pool<test_object> chunkPool(16);
 
-	gdul::chunk_allocator<int*, decltype(chunkPool)> allocator(chunkPool);
+	gdul::chunk_allocator<test_object, decltype(chunkPool)> allocator(chunkPool);
 
 	gdul::shared_ptr<test_object> sp = gdul::allocate_shared<test_object>(allocator);
 
