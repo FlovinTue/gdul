835b038822f388029a33b0743e0861602945d469
Flovin Tue Michaelsen <flovintue@outlook.com>
2020-04-05 08:57:48 +0200

    Added first sketch

---
+++ job_handler_plot_view/job_handler_plot_view/job_handler_plot_view.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace job_handler_plot_view
+{
+    partial class job_handler_plot_view
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.DataSource = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(152, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(636, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(628, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Avg";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(628, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Min";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(628, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Max";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // DataSource
+            // 
+            this.DataSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.DataSource.FormattingEnabled = true;
+            this.DataSource.Items.AddRange(new object[] {
+            "CompletionTime(job)",
+            "CompletionTime(dep.chain)",
+            "WaitTime(working)",
+            "WaitTime(waiting)"});
+            this.DataSource.Location = new System.Drawing.Point(12, 34);
+            this.DataSource.Name = "DataSource";
+            this.DataSource.Size = new System.Drawing.Size(134, 394);
+            this.DataSource.TabIndex = 1;
+            this.DataSource.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = "ListBoxName";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Data Source";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // job_handler_plot_view
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DataSource);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "job_handler_plot_view";
+            this.Text = "job_handler_plot_view";
+            this.Load += new System.EventHandler(this.job_handler_plot_view_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ListBox DataSource;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
