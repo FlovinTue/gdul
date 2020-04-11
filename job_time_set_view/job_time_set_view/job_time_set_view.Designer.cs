59804843f8c5be45301cf2d164aefd2ebbd677a5
Flovin Tue Michaelsen <flovintue@outlook.com>
2020-04-11 08:11:26 +0200

    Added first chart

--- job_time_set_view/job_time_set_view/job_time_set_view.Designer.cs
+++ job_time_set_view/job_time_set_view/job_time_set_view.Designer.cs
@@ -32,21 +32,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.ItemProperties = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DataSource
             // 
-            this.DataSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.DataSource.FormattingEnabled = true;
             this.DataSource.Location = new System.Drawing.Point(12, 34);
             this.DataSource.Name = "DataSource";
-            this.DataSource.Size = new System.Drawing.Size(134, 602);
+            this.DataSource.Size = new System.Drawing.Size(239, 212);
             this.DataSource.TabIndex = 1;
             this.DataSource.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -54,7 +48,7 @@
             // 
             this.label1.AccessibleDescription = "DataSourceLabel";
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 18);
+            this.label1.Location = new System.Drawing.Point(98, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 2;
@@ -65,11 +59,11 @@
             // 
             this.ItemProperties.AccessibleDescription = "Item properties";
             this.ItemProperties.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.ItemProperties.FormattingEnabled = true;
-            this.ItemProperties.Location = new System.Drawing.Point(833, 34);
+            this.ItemProperties.Location = new System.Drawing.Point(12, 270);
             this.ItemProperties.Name = "ItemProperties";
-            this.ItemProperties.Size = new System.Drawing.Size(201, 602);
+            this.ItemProperties.Size = new System.Drawing.Size(239, 420);
             this.ItemProperties.TabIndex = 3;
             this.ItemProperties.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
             // 
@@ -77,68 +71,32 @@
             // 
             this.label2.AccessibleDescription = "ItemPropertyLabel";
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(895, 18);
+            this.label2.Location = new System.Drawing.Point(93, 254);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Item Properties";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(664, 619);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Max";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(664, 619);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Min";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
-            // 
             // tabControl1
             // 
             this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(152, 12);
+            this.tabControl1.Location = new System.Drawing.Point(260, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(672, 645);
+            this.tabControl1.Size = new System.Drawing.Size(739, 695);
             this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(664, 619);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Avg";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
             // 
             // job_time_set_view
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1044, 666);
+            this.ClientSize = new System.Drawing.Size(1011, 716);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.ItemProperties);
             this.Controls.Add(this.label1);
@@ -149,7 +107,6 @@
             this.Load += new System.EventHandler(this.job_time_set_view_Load);
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.job_time_set_view_on_drag_drop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.job_time_set_view_drag_enter);
-            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,10 +117,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox ItemProperties;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
     }
 }
 
--- job_time_set_view/job_time_set_view/job_time_set_view.cs
+++ job_time_set_view/job_time_set_view/job_time_set_view.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace job_time_set_view
 {
@@ -26,12 +27,12 @@ namespace job_time_set_view
 
         public job_time_set_view()
         {
+            InitializeComponent();
+
             m_jobInfos = new Dictionary<string, job_info>();
             m_dataSources = new Dictionary<string, Dictionary<string, List<Tuple<float, job_info>>>>();
 
             m_enCultureInfo = new System.Globalization.CultureInfo("en-US");
-
-            InitializeComponent();
         }
 
         private void job_time_set_view_on_drag_drop(object sender, System.Windows.Forms.DragEventArgs e)
@@ -55,6 +56,9 @@ namespace job_time_set_view
             newDoc.Load(first);
 
             parse_xml(newDoc);
+
+            foreach (var dataSource in m_dataSources)
+                this.DataSource.Items.Add(dataSource.Key);
         }
         private void job_time_set_view_drag_enter(object sender, System.Windows.Forms.DragEventArgs e)
         {
@@ -118,9 +122,82 @@ namespace job_time_set_view
                 }
             }
         }
+        private void generate_tabs(string dataSource)
+        {
+            if (!m_dataSources.ContainsKey(dataSource))
+                return;
+
+            var ds = m_dataSources[dataSource];
+
+            foreach (var variationSource in ds)
+            {
+                TabPage page = new TabPage();
+                page.Location = new System.Drawing.Point(4, 22);
+                page.Size = new System.Drawing.Size(846, 669);
+                page.UseVisualStyleBackColor = true;
+                page.Text = variationSource.Key;
+
+                Chart chart = new Chart();
+
+                ((System.ComponentModel.ISupportInitialize)(chart)).BeginInit();
+
+                chart.Location = new System.Drawing.Point(4, 22);
+                chart.Size = new System.Drawing.Size(846, 669);
+                chart.TabIndex = 5;
+
+                ChartArea chartArea = new ChartArea();
+                chartArea.Name = "ChartArea1";
+                chart.ChartAreas.Add(chartArea);
+
+                Legend legend = new Legend();
+                legend.Name = "Legend1";
+                chart.Legends.Add(legend);
+
+                Series series = new Series();
+                series.Name = "Series1";
+                series.Legend = "Legend1";
+                series.ChartArea = "ChartArea1";
+
+                foreach(var data in variationSource.Value)
+                {
+                    series.Points.Add(data.Item1);
+                }
+
+                chart.Series.Add(series);
+
+                ((System.ComponentModel.ISupportInitialize)(chart)).EndInit();
+
+                page.Controls.Add(chart);
+
+                this.tabControl1.TabPages.Add(page);
+            }
+
+
+            //chartArea1.Name = "ChartArea1";
+            //this.chart1.ChartAreas.Add(chartArea1);
+            //legend1.Name = "Legend1";
+            //this.chart1.Legends.Add(legend1);
+            //this.chart1.Location = new System.Drawing.Point(3, 3);
+            //this.chart1.Name = "chart1";
+            //series1.ChartArea = "ChartArea1";
+            //series1.Legend = "Legend1";
+            //series1.Name = "Series1";
+            //this.chart1.Series.Add(series1);
+            //this.chart1.Size = this.tabControl1.Size;
+            //this.chart1.TabIndex = 0;
+            //this.chart1.Text = "chart1";
+        }
+
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            this.tabControl1.TabPages.Clear();  
 
+            var selectedItem = this.DataSource.SelectedItem;
+
+            if (selectedItem == null)
+                return;
+
+            generate_tabs(selectedItem as string);
         }
 
         private void job_time_set_view_Load(object sender, EventArgs e)
@@ -128,25 +205,7 @@ namespace job_time_set_view
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void tabPage2_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void tabPage3_Click(object sender, EventArgs e)
         {
 
         }
@@ -161,9 +220,5 @@ namespace job_time_set_view
 
         }
 
-        private void chart1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
