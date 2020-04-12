f8313598538c390b51db2020265a4fee33d5a356
Flovin Tue Michaelsen <flovintue@outlook.com>
2020-04-12 19:45:20 +0200

    Renamed -> pool_allocator

--- Testers/pool_allocator/pool_allocator/Main.cpp
+++ Testers/pool_allocator/pool_allocator/Main.cpp
@@ -4,7 +4,7 @@
 #include "pch.h"
 #include <iostream>
 #include <atomic>
-#include <gdul/WIP_chunk_allocator/chunk_allocator.h>
+#include <gdul/WIP_pool_allocator/pool_allocator.h>
 #include <vld.h>
 
 
@@ -16,12 +16,12 @@ struct alignas(32) test_object
 
 int main()
 {
-	gdul::memory_chunk_pool pool;
+	gdul::memory_pool pool;
 
-	constexpr std::size_t allocSharedSize(gdul::allocate_shared_size<test_object, gdul::chunk_allocator<test_object>>());
+	constexpr std::size_t allocSharedSize(gdul::allocate_shared_size<test_object, gdul::pool_allocator<test_object>>());
 	pool.init<allocSharedSize, alignof(test_object)>(8);
 
-	gdul::chunk_allocator<test_object> alloc(pool.create_allocator<test_object>());
+	gdul::pool_allocator<test_object> alloc(pool.create_allocator<test_object>());
 
 	gdul::shared_ptr<test_object> sp(gdul::allocate_shared<test_object>(alloc));
 
