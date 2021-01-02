e09f70fae41213cbc027981149e42aa100dea321
Flovin Tue Michaelsen <flovintue@outlook.com>
2021-01-02 19:54:03 +0100

    Added some comments, support methods and perf optimization

--- Testers/concurrent_skip_list/main.cpp
+++ Testers/concurrent_skip_list/main.cpp
@@ -15,6 +15,7 @@
 #include <mutex>
 
 constexpr uint32_t inserts = 256 * 8;
+constexpr size_t iterations(1000000);
 
 struct container
 {
@@ -22,49 +23,56 @@ struct container
 	gdul::concurrent_skip_list<uint32_t, uint32_t, inserts> sl;
 	std::vector<uint32_t> keys;
 	gdul::concurrent_hash_map<uint32_t, uint32_t> hm = gdul::concurrent_hash_map<uint32_t, uint32_t>(inserts);
+	//concurrency::concurrent_unordered_set<int> us;
+	std::unordered_map<int, int> us;
 };
 
-//concurrency::concurrent_unordered_set<int> us;
 gdul::shared_ptr<container> c;
 
 std::mutex mtx;
-//std::unordered_set<int> us;
 
-
+thread_local size_t nonsense = 0;
+size_t global = 0;
 void find()
 {
 	const decltype(c->sl)& sl(c->sl);
 	sl;
 
-	for (std::size_t i = 0; i < c->keys.size();++i) {
-		//auto itr(us.find(c->keys[i]));
-		//sl.find(c->keys[i]) != sl.end();
-		c->hm.find(c->keys[i]);
+	for (auto& itr : c->keys) {
+		//auto result(c->us.find(itr));
+		//sl.find(itr) != sl.end();
+		auto result(c->hm.find(itr));
+
+		nonsense += (*result).second;
+
 	}
+	global += nonsense;
 }
-void insert()
+void insert(int ix)
 {
+	mtx.lock();
 	std::vector<uint32_t> keys;
 	for (uint32_t i = 0; i < inserts / std::thread::hardware_concurrency(); ++i) {
-		const uint32_t k(gdul::csl_detail::t_rng());
+		const uint32_t k(gdul::csl_detail::t_rng() + ix);
 		keys.push_back(k);
 		c->sl.insert({ k, k });
-		//us.insert(k);
+		c->us.insert({ k, k });
 		c->hm.insert({ k,k });
 	}
 
-	mtx.lock();
 	for (auto k : keys) {
 		c->keys.push_back(k);
 	}
 	mtx.unlock();
 }
+
+
 int main()
 {
 	c = gdul::make_shared<container>();
 
 	for (int i = 0; i < 8; ++i) {
-		c->tp.add_task([]() {insert(); });
+		c->tp.add_task([i]() {insert(i); });
 	}
 
 	while (c->tp.has_unfinished_tasks()) {
@@ -73,10 +81,11 @@ int main()
 
 	for (int i = 0; i < c->keys.size(); ++i) {
 		GDUL_ASSERT(c->sl.find(c->keys[i]) != c->sl.end() && "Failed at");
+		GDUL_ASSERT(c->hm.find(c->keys[i]) != c->hm.end() && "Failed at");
 	}
 
 	gdul::timer<float> t;
-	for (std::size_t i = 0; i < 100000; ++i) {
+	for (std::size_t i = 0; i < iterations; ++i) {
 		c->tp.add_task([]() { find(); });
 	}
 
@@ -84,5 +93,12 @@ int main()
 		std::this_thread::yield();
 	}
 
-	std::cout << 100000 * inserts << " finds took: " << t.get() << " seconds" << std::endl;
+	for (auto& itr : c->keys) {
+		GDUL_ASSERT(c->hm.unsafe_erase(itr) == true);
+		GDUL_ASSERT(c->hm.find(itr) == c->hm.end());
+	}
+
+
+	std::cout << iterations * inserts << " finds took: " << t.get() << " seconds" << std::endl;
+	std::cout << global << std::endl;
 }
