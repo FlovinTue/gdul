002c457a6c91c0527ae7d00946dfe45b3ff7af7f
Flovin Tue Michaelsen <flovintue@outlook.com>
2021-01-31 12:19:56 +0100

    Added some notices

--- source/gdul/memory/concurrent_scratch_pool.h
+++ source/gdul/memory/concurrent_scratch_pool.h
@@ -1,4 +1,4 @@
-// Copyright(c) 2020 Flovin Michaelsen
+// Copyright(c) 2021 Flovin Michaelsen
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files(the "Software"), to deal
